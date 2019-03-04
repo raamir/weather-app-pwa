790ec29f7b2cb14fb5c8310ad730b2c6aa551951
raamir <raamir@mail.depaul.edu>
2019-03-04 12:06:39 -0500

    Initial commit from gatsby: (https://github.com/raamir/gatsbynew-weather-app.git)

---
+++ gatsby-config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Weather App`,
+  },
+  plugins: [
+
+    {
+      resolve: 'gatsby-source-openweathermap',
+      options: {
+        apikey: '45a7db89e2910b3f2bb75ea7a0bd5eb8',
+        location: 'New York',
+        units: 'imperial',
+        type: 'weather'
+      },
+    },
+
+    {
+      resolve: 'gatsby-source-darksky',
+      options: {
+        key: '46da3b60101841b35a75ab3bf89b4de7',
+        latitude: '43.09',
+        longitude: '-84.99',
+        exclude: ['minutely']
+      },
+    }
+
+  ]
+}; 
